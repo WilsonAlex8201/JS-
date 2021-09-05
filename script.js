3634f962a2968948286047e913b4adea60af5ad9
Alex Wilson <wilsonalex8201@gmail.com>
2021-09-05 16:47:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict";
+
+
+
+// ^
+// 2.10
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == 9);
+// console.log(2+2*4 != 8);
+// console.log(2*4 === '8');
+
+// // && - и
+// // || - или
+// // !  - инвертор
+
+// const isChecked = true,
+//       isClose = true;
+
+// console.log(isChecked && isClose);
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked || isClose);
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && !isClose);
+
+// ^
+// 2.9
+// const category = "toys";
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = "Alex";
+// alert(`Hello, ${user}`)
+
+// ^
+// 2.8
+
+// alert('Hello')
+
+// const result = confirm("You eblan?")
+// console.log(result);
+
+// const answer = +prompt("Do you is 18 old?", "?")
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("how your name?");
+// answers[1] = prompt("how your surname?");
+// answers[2] = prompt("how much your year?");
+
+// document.write(answers);
